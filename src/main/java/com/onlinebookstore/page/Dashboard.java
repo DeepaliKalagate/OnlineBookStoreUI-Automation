bfe76171b962e11345b9a350e502184dd09a41e3
khairnarswapna <khairnarswapna99@gmail.com>
2020-01-25 17:51:06 +0530

    Added: add testcase for selecting the books from dashboad

--- src/main/java/com/onlinebookstore/page/Dashboard.java
+++ src/main/java/com/onlinebookstore/page/Dashboard.java
@@ -1,8 +1,43 @@
 package com.onlinebookstore.page;
 
 import com.onlinebookstore.base.Browser;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 public class Dashboard extends Browser
 {
 
+    @FindBy(xpath="//div[6]//div[1]//div[1]//button[1]")
+    private WebElement Book6;
+
+
+    @FindBy(xpath="//div[15]//div[1]//div[1]//button[1]")
+    private WebElement Book15;
+
+    @FindBy(xpath="//div[51]//div[1]//div[1]//button[1]")
+    private WebElement Book51;
+
+    @FindBy(xpath="//div[27]//div[1]//div[1]//button[1]")
+    private WebElement Book27;
+
+    //initialization
+    public Dashboard(WebDriver driver)
+    {
+        PageFactory.initElements(driver, this);
+    }
+
+    //utilization
+    public void clickAddToBag() throws InterruptedException {
+
+        Book6.click();
+        Thread.sleep(4000);
+        Book15.click();
+        Thread.sleep(4000);
+        Book51.click();
+        Thread.sleep(4000);
+        Book27.click();
+        Thread.sleep(4000);
+    }
 }
---
+++ src/test/java/com/onlinebookstore/script/SelectBookTest.java
@@ -0,0 +1,18 @@
+package com.onlinebookstore.script;
+
+import com.onlinebookstore.base.Browser;
+import com.onlinebookstore.page.Dashboard;
+import org.testng.annotations.Test;
+
+public class SelectBookTest extends Browser {
+    @Test
+    public void testName() throws InterruptedException {
+
+        Dashboard  dashboard = new Dashboard(driver);
+        Thread.sleep(4000);
+        dashboard.clickAddToBag();
+        Thread.sleep(4000);
+
+    }
+
+}
